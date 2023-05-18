e6a752b171394ed3bd1df4d5191798463829bb6e
YOUNGSEOK SEO <sysconan@gmail.com>
2023-05-19 01:01:20 +0900

    230519

--- sm/segmentation_model/tools/train_model.py
+++ sm/segmentation_model/tools/train_model.py
@@ -111,42 +111,42 @@ def area_segmentation(X_train, y_train, X_val, y_val, X_test, y_test, classes=['
         def __len__(self):
             return len(self.ids)
 
-    class predict_Dataset:
+#     class predict_Dataset:
 
-        def __init__(
-                self, 
-                images,
-                augmentation=None, 
-                preprocessing=None,
-        ):
-#             self.ids = os.listdir(images_dir)
-#             self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
-            self.images = images
+#         def __init__(
+#                 self, 
+#                 images,
+#                 augmentation=None, 
+#                 preprocessing=None,
+#         ):
+# #             self.ids = os.listdir(images_dir)
+# #             self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+#             self.images = images
 
-            # convert str names to class values on masks
-            self.augmentation = augmentation
-            self.preprocessing = preprocessing
+#             # convert str names to class values on masks
+#             self.augmentation = augmentation
+#             self.preprocessing = preprocessing
 
-        def __getitem__(self, i):
+#         def __getitem__(self, i):
 
-            # read data
-    #         image = cv2.imread(self.images_fps[i])
-            image = cv2.cvtColor(self.images, cv2.COLOR_BGR2RGB)
+#             # read data
+#     #         image = cv2.imread(self.images_fps[i])
+#             image = cv2.cvtColor(self.images, cv2.COLOR_BGR2RGB)
 
-            # apply augmentations
-            if self.augmentation:
-                sample = self.augmentation(image=image)
-                image = sample['image']
+#             # apply augmentations
+#             if self.augmentation:
+#                 sample = self.augmentation(image=image)
+#                 image = sample['image']
 
-            # apply preprocessing
-            if self.preprocessing:
-                sample = self.preprocessing(image=image)
-                image = sample['image']
+#             # apply preprocessing
+#             if self.preprocessing:
+#                 sample = self.preprocessing(image=image)
+#                 image = sample['image']
 
-            return image
+#             return image
 
-        def __len__(self):
-            return len(self.ids)
+#         def __len__(self):
+#             return len(self.ids)
 
 
     class Dataloder(keras.utils.Sequence):
